c8790344c15142f02fa41d86bd36393db732bf6b
jasleen8713 <jasleen8713@gmail.com>
2018-10-01 17:51:18 +0200

    show map with input address

--- server.R
+++ server.R
@@ -1,8 +1,29 @@
 shinyServer(function(input, output, session){
   
-  api_key <- ""
+  observeEvent(input$text,{
+    hide(id = "mapBoxdiv", anim = TRUE)
+    if (input$text == ""){
+      return(NULL)
+    }
+    else
+    {
+      output$map <- renderGoogle_map({
+      google_map(key = input$text) %>%
+          clear_markers()
+      })
+      show(id = "mapBoxdiv", anim = TRUE)
+    }
+
+  })
   
-  output$map <- renderGoogle_map({
-    google_map(key = api_key)
+  observeEvent(input$buttonGetGeocode, {
+    #call api function
+    returnedgeocode <- Geocode(input$address)
+    df <- data.frame(lat=returnedgeocode[1],lon=returnedgeocode[2])
+    output$map <- renderGoogle_map({
+      google_map(key = input$text, location = returnedgeocode, data= df) %>%
+        add_markers(data = df, lat="lat",lon="lon", update_map_view = F)
+    })
+   
   })
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,17 @@
 library(shiny)
 library(googleway)
+library(shinydashboard)
+library(shinyjs)
+library(httr) #for api call
+library(jsonlite) # for api call
 
-shinyUI(fluidPage(google_mapOutput("map")))
+shinyUI(fluidPage(
+  useShinyjs(),
+  textInput("text", label = h3("Submit API Key"), value = "", width="400px"),
+  textInput("address", label = h3("Type address / Geo codes"), value = "", width="400px"),
+  actionButton("buttonGetGeocode","Get Geo Code"),
+  div(id="mapBoxdiv",
+  box(id = "mapBox", title = "Address Output", width = "800px", height="800px", google_mapOutput(outputId = "map")))
+  ))
 
 
