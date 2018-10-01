a64cf0e6f2d976cd200fb795f09fc675a823cffd
jasleen8713 <jasleen8713@gmail.com>
2018-10-02 01:01:29 +0200

    Slider panel and css

--- server.R
+++ server.R
@@ -18,12 +18,29 @@ shinyServer(function(input, output, session){
   
   observeEvent(input$buttonGetGeocode, {
     #call api function
-    returnedgeocode <- Geocode(input$address)
-    df <- data.frame(lat=returnedgeocode[1],lon=returnedgeocode[2])
+    returnedgeocode <- APILab5::latlon(input$address, input$text)
+    df <- data.frame(lat=returnedgeocode[2],lon=returnedgeocode[1])
+    print(df)
+    output$printGeoCodes <- renderText(paste("lat",returnedgeocode[2],"lon",returnedgeocode[1]))
     output$map <- renderGoogle_map({
-      google_map(key = input$text, location = returnedgeocode, data= df) %>%
+      google_map(key = input$text, location = c(returnedgeocode[2],returnedgeocode[1]), data= df) %>%
         add_markers(data = df, lat="lat",lon="lon", update_map_view = F)
     })
    
+  })
+  
+  observeEvent(input$buttonLatLon, {
+    #call api function
+    locationVector <- c(as.numeric(input$lat), as.numeric(input$lon))
+    print(locationVector)
+    df <- data.frame(lat=locationVector[1],lon=locationVector[2])
+    print(df)
+    returnedAddress <- APILab5::reverselatlon(input$lat, input$lon, input$text)
+    output$printGeoCodes <- renderText(returnedAddress)
+    output$map <- renderGoogle_map({
+      google_map(key = input$text, location = locationVector, data= df) %>%
+        add_markers(data = df, lat="lat",lon="lon", update_map_view = F)
+    })
+    
   })
 })
--- ui.R
+++ ui.R
@@ -6,12 +6,47 @@ library(httr) #for api call
 library(jsonlite) # for api call
 
 shinyUI(fluidPage(
+  
+  tags$head(
+    tags$style(HTML("
+@import url('//fonts.googleapis.com/css?family=Roboto');
+h1{
+font-family: 'Roboto';
+color: #48ca3b;
+}
+
+
+"))
+),
+  
+  headerPanel("Interface using Google Geocode API"),
   useShinyjs(),
-  textInput("text", label = h3("Submit API Key"), value = "", width="400px"),
-  textInput("address", label = h3("Type address / Geo codes"), value = "", width="400px"),
-  actionButton("buttonGetGeocode","Get Geo Code"),
-  div(id="mapBoxdiv",
-  box(id = "mapBox", title = "Address Output", width = "800px", height="800px", google_mapOutput(outputId = "map")))
+  sidebarLayout(
+    
+    sidebarPanel(
+      textInput("text", label = h5("Enter API Key"), value = "", width="400px"),
+      textInput("address", label = h5("Type address"), value = "", width="400px"),
+      actionButton("buttonGetGeocode","Get Geo Code", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+      br(),
+      br(),
+      br(),
+      sliderInput("lat", "Latitude", min = -90, max = 90, value = 0,step = .00001),
+      sliderInput("lon", "Longitude", min = -180, max = 180, value = 0,step = .00001),
+      
+      #textInput("lat",label = h5("Latitude"), value = "", width="100px"),
+      #textInput("lon",label = h5("Longitude"), value = "", width="100px"),
+      actionButton("buttonLatLon","Get Address", style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+      width=6
+    ),
+    
+    mainPanel(width=6,
+      div(id="mapBoxdiv",
+          verbatimTextOutput("printGeoCodes",placeholder = TRUE),
+          box(id = "mapBox", width = "1000px", height="1000px", google_mapOutput(outputId = "map")))
+    )
+  )
+  
+ 
   ))
 
 
